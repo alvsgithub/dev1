c1d667de0ea449ca7c3d8fe70e75b7e64ab99e43
firmantok <fir_man_tok@yahoo.com>
2014-03-12 10:35:17 +0700

    update view anggaran index

--- application/views/app/master/anggaran/index.php
+++ application/views/app/master/anggaran/index.php
@@ -2,12 +2,21 @@
     <h4>Data Anggaran - Item : <?php echo $jenis; ?></h4>
 </div>
 <section_custom>
-    <?php echo btn_add('app/anggaran/edit/'.$jenis.'-new'); ?>
+
+    <span id="labelPeriode">Periode : </span>
+    <select id="id_periode" name="id_periode" style="width: 100px;">
+        <?php foreach($options_periode as $periode) { ?>
+            <option value="<?php echo $periode->tahun.'0'.$periode->semester; ?>"><?php echo $periode->tahun.'0'.$periode->semester; ?></option>
+        <?php } ?>
+    </select>
+    <span id="space"> &nbsp;&nbsp;&nbsp; </span>
+    <?php echo btn_add('app/anggaran/edit/'.$jenis.'-new'); ?>    
+    
     <table id="example" class="table table-striped table-bordered table-hover table-condensed">
         <thead>
             <tr id="header1">
                 <th>Kode</th>
-                <th>Periode</th>
+                <th style="display: none;">Periode</th>
                 <th>Nama</th>
                 <th>Satuan</th>
                 <th>Harga Pagu</th>
@@ -19,7 +28,7 @@
         <?php if(count($item)): foreach($item as $it): ?>	
             <tr>
                 <td><?php echo anchor('app/anggaran/edit/' . $jenis .'-'. $it->id, $it->kode); ?></td>
-                <td><?php echo $it->periode; ?></td>
+                <td style="display: none;"><?php echo $it->periode; ?></td>
                 <td><?php echo $it->nama; ?></td>
                 <td><?php echo $it->satuan; ?></td>
                 <td style="text-align: right;"><?php echo number_format($it->harga_pagu, 2, ',', '.'); ?></td>
@@ -32,11 +41,10 @@
         <?php endforeach; ?>
         <?php else: ?>
                 <tr>
-                    <td colspan="3">We could not find any data.</td>
+                    <td colspan="7">We could not find any data.</td>
                 </tr>
         <?php endif; ?>	
         </tbody>
-        
     </table>
     
 </section_custom>
